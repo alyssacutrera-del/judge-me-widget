638edae4daa339dcf77ea92883f75bbe7fbd4d1b
alyssacutrera-del <alyssacutrera@gmail.com>
2026-03-09 15:05:56 -0500

    jdgm-reviews.js

--- jdgm-reviews.js
+++ jdgm-reviews.js
@@ -1,1 +1,15 @@
-document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+class JdgmReviews extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <div class="jdgm-widget jdgm-all-reviews-widget">
+        <div class="jdgm-all-reviews__body"></div>
+      </div>
+    `;
+
+    if (window.jdgm && typeof window.jdgm.init === "function") {
+      window.jdgm.init();
+    }
+  }
+}
+
+customElements.define("jdgm-reviews", JdgmReviews);
