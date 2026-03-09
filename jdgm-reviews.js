504b5db5a638b5b9e522a1f6bbd762592d5cd5e5
alyssacutrera-del <alyssacutrera@gmail.com>
2026-03-09 15:12:29 -0500

    jdgm-reviews.js

--- jdgm-reviews.js
+++ jdgm-reviews.js
@@ -1,14 +1,23 @@
 class JdgmReviews extends HTMLElement {
   connectedCallback() {
+    this.style.display = "block";
+    this.style.minHeight = "1200px";
+
     this.innerHTML = `
       <div class="jdgm-widget jdgm-all-reviews-widget">
         <div class="jdgm-all-reviews__body"></div>
       </div>
     `;
 
-    if (window.jdgm && typeof window.jdgm.init === "function") {
-      window.jdgm.init();
-    }
+    const initJudgeMe = () => {
+      if (window.jdgm && typeof window.jdgm.init === "function") {
+        window.jdgm.init();
+      } else {
+        setTimeout(initJudgeMe, 300);
+      }
+    };
+
+    initJudgeMe();
   }
 }
 
